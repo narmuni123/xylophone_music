2aaf1399f83c348453a54a2c3acfb4b42536c644
Munikiran Narpareddi <narmuni123@gmail.com>
2022-06-06 12:37:26 +0530

    final project with audio on tap.

--- lib/screens/xylophone_screen.dart
+++ lib/screens/xylophone_screen.dart
@@ -12,33 +12,35 @@ class Xylophone extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            soundButton('assets/', 'assets_note1.wav', Colors.red),
-            soundButton('assets/', 'assets_note2.wav', Colors.black),
-            soundButton('assets/', 'assets_note3.wav', Colors.blue),
-            soundButton('assets/', 'assets_note4.wav', Colors.green),
-            soundButton('assets/', 'assets_note5.wav', Colors.yellow),
-            soundButton('assets/', 'assets_note6.wav', Colors.orange),
-            soundButton('assets/', 'assets_note7.wav', Colors.teal),
+            soundButton(context, 'assets/', 'assets_note1.wav', Colors.red),
+            soundButton(context, 'assets/', 'assets_note2.wav', Colors.black),
+            soundButton(context, 'assets/', 'assets_note3.wav', Colors.blue),
+            soundButton(context, 'assets/', 'assets_note4.wav', Colors.green),
+            soundButton(context, 'assets/', 'assets_note5.wav', Colors.yellow),
+            soundButton(context, 'assets/', 'assets_note6.wav', Colors.orange),
+            soundButton(context, 'assets/', 'assets_note7.wav', Colors.teal),
           ],
         ),
       ),
     );
   }
 
-  Widget soundButton(String prefix, String note, Color color){
-    return TextButton(
-      onPressed: () async{
-        final play = AudioCache(prefix: prefix);
-        final url = await play.load(note);
-        audioPlayer.setUrl(url.path, isLocal: true);
-        await audioPlayer.play(url.path);
-      },
-      child: Container(
-        height: 50,
-        width: 50,
-        color: color,
+  Widget soundButton(BuildContext context, String prefix, String note, Color color){
+    final size = MediaQuery.of(context).size;
+    return Expanded(
+      child: TextButton(
+        onPressed: () async{
+          final play = AudioCache(prefix: prefix);
+          final url = await play.load(note);
+          audioPlayer.setUrl(url.path, isLocal: true);
+          await audioPlayer.play(url.path);
+        },
+        child: Container(
+          height: size.height,
+          width: size.width,
+          color: color,
+        ),
       ),
     );
   }
