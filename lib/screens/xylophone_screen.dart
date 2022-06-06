fa85c2ded893a67c20ff90844f80a7659a2123cb
Munikiran Narpareddi <narmuni123@gmail.com>
2022-06-06 11:05:42 +0530

    added the first note for testing

--- lib/screens/xylophone_screen.dart
+++ lib/screens/xylophone_screen.dart
@@ -2,10 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 class Xylophone extends StatelessWidget {
-  const Xylophone({Key? key}) : super(key: key);
+   Xylophone({Key? key}) : super(key: key);
+
+  final audioPlayer = AudioPlayer();
+
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: TextButton(
+            onPressed: () async{
+              final play = AudioCache(prefix: 'assets/');
+              final url = await play.load("assets_note1.wav");
+              audioPlayer.setUrl(url.path, isLocal: true);
+              await audioPlayer.play(url.path);
+            },
+            child: const Text("Click Me"),
+          ),
+        ),
+      ),
+    );
   }
 }
