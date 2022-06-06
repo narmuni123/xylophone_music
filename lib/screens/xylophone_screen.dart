343ec622337d9e9761e9cf0135601a3076f3c5ff
Munikiran Narpareddi <narmuni123@gmail.com>
2022-06-06 11:25:09 +0530

    added all the seven notes

--- lib/screens/xylophone_screen.dart
+++ lib/screens/xylophone_screen.dart
@@ -11,17 +11,34 @@ class Xylophone extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: TextButton(
-            onPressed: () async{
-              final play = AudioCache(prefix: 'assets/');
-              final url = await play.load("assets_note1.wav");
-              audioPlayer.setUrl(url.path, isLocal: true);
-              await audioPlayer.play(url.path);
-            },
-            child: const Text("Click Me"),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            soundButton('assets/', 'assets_note1.wav', Colors.red),
+            soundButton('assets/', 'assets_note2.wav', Colors.black),
+            soundButton('assets/', 'assets_note3.wav', Colors.blue),
+            soundButton('assets/', 'assets_note4.wav', Colors.green),
+            soundButton('assets/', 'assets_note5.wav', Colors.yellow),
+            soundButton('assets/', 'assets_note6.wav', Colors.orange),
+            soundButton('assets/', 'assets_note7.wav', Colors.teal),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget soundButton(String prefix, String note, Color color){
+    return TextButton(
+      onPressed: () async{
+        final play = AudioCache(prefix: prefix);
+        final url = await play.load(note);
+        audioPlayer.setUrl(url.path, isLocal: true);
+        await audioPlayer.play(url.path);
+      },
+      child: Container(
+        height: 50,
+        width: 50,
+        color: color,
       ),
     );
   }
